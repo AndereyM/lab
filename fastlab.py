e36ab997aa9ff165018541c8a1a79aa745785746
AndereyM <142475228+AndereyM@users.noreply.github.com>
2023-10-07 09:11:54 +0000

    fastlab.py

--- fastlab.py
+++ fastlab.py
@@ -1,8 +1,8 @@
+from fastapi import FastAPI24
 app = FastAPI()
 def sum_two_args(x,y):
- return x+y
+return x+y
 # Hello World route
 @app.get("/")
 def read_root():
- return {"Hello": "World"}
-
+return {"Hello": "World"}
